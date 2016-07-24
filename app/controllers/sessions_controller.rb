59c109558159db6a5d911e049e0e3fa9c8df9b8e
nick bushell <email": null>
2016-07-24 13:51:29 -0700

    finished skeleton

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+session_helper.rbget '/sessions/new' do
+  erb :"/sessions/new"
+end
+
+post '/sessions' do
+  @patient = Patient.find_by(email: params[:user][:email])
+
+  if @patient && @patient.authenticate(params[:user][:password])
+    session[:id] = @patient.id
+    redirect "/patients/#{session[:id]}"
+  else
+    @error = "Incorrect login info"
+    redirect "/sessions/new"
+  end
+end
+
+get '/sessions/logout' do
+  session[:user_id] = nil
+  current_patient = nil
+  redirect '/'
+end
--- app/db/seeds.rb
+++ app/db/seeds.rb
@@ -1,1 +1,14 @@
+require 'faker'
 
+
+10.times do
+
+
+  Patient.create(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    email: Faker::Internet.email,
+    password_digest: Faker::Internet.password(8, 20)
+    )
+
+end
